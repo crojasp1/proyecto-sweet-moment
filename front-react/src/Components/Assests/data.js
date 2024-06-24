b8c3253e6f2d52a38989b4a4d72d81d1de93b5d9
unknown <amontero@gmail.com>
2024-06-23 21:27:24 -0500

    actualizar

---
+++ front-react/src/Components/Assests/data.js
@@ -0,0 +1,33 @@
+import p1_img from './product_1'
+import p2_img from './product_2'
+import p3_img from './product_3'
+import p4_img from './product_4'
+
+let data_product = [
+    {
+        id:1,
+        name:"enfermera",
+        image:p1_img,
+        new_price:50.00,
+        ald_price:80.50,
+    },
+    {id:2,
+        name:"colegiala",
+        image:p2_img,
+        new_price:85.00,
+        old_price:120,50.
+    },
+    {id:3,
+        name:"mucama",
+        image:p3_img,
+        new_price:55.00,
+        old_price:320,0.
+    },
+    {id:4,
+        name:"marinera",
+        image:p4_img,
+        new_price:15.00,
+        old_price:30,0.
+    },
+]
+
---
+++ front-react/src/Components/Popular/Popular.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'Popular.css'
+import data_product from '../Assests/data'
+import Item from '../Item/item'
+
+const Popular = () => {
+  return (
+        <div className='popular'>
+        <h1>POPULAR IN WOMEN</h1>
+        <hr />
+        <div className="popular-item">
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={Item.image} new_price={item.new_price_price} old_price={item.old_price}/>
+            })}
+        </div>
+        </div>
+  )
+}
+
+export default Popular
