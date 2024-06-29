12ad4c74ea78bbb788d63a2242c826c82b619f0c
crojasp1 <camilorojasprieto@gmail.com>
2024-06-29 14:40:02 -0500

    NewsLetter and Footer components created. Footer is gonna be shown in every category because it is imported to App.js document not to Shop that is our main page and as we do with the other components. NewsLetter is for the user and it ask for an email to send extra info and get a siubscriber.

--- front-react/src/Pages/Shop.jsx
+++ front-react/src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Footer from "../Components/Footer/Footer";
 
 const Shop = () => {
   return (
@@ -9,6 +11,7 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewsLetter/>
     </div>
   )
 }
